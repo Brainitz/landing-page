660c4e5bb2c7edf1440d92f82d8df41d239183ca
MIchael <michael.olson756@gmail.com>
2017-04-27 22:04:25 -0600

    updated splash

--- js/firebase.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  apiKey: "AIzaSyA51QX2kHcx4Bl3Ui0zcKGI22QfhtSBgQY",
-  authDomain: "brainitz-email.firebaseapp.com",
-  databaseURL: "https://brainitz-email.firebaseio.com",
-  storageBucket: "brainitz-email.appspot.com",
-  messagingSenderId: "240557418065"
-};
-var fb = firebase.initializeApp(config);
